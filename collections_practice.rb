6336abba0eacb6e37e5d29b2d6de7634189e4ecc
David Kuruvilla <dapawn@gmail.com>
2017-10-19 17:44:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,4 +4,17 @@ end
 
 def sort_array_desc(arr)
   arr.sort.reverse
+end 
+
+def sort_array_char_count(arr)
+  arr.sort do |a, b|
+  if a.length == b.length
+    0
+  elsif a.length < b.length
+    -1
+  elsif a.length > b.length
+    1
+  end
+end
+
 end
